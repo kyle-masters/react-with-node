8da8f91e8de051cef139c9511294fd7bc31529ed
Kyle Masters <29558349+kyle-masters@users.noreply.github.com>
2023-03-16 21:44:58 -0600

    removed inappropriate

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ ur: 'a bitch' });
+    res.send({ hello: 'world' });
 });
 
 const PORT = process.env.PORT || 5000;
